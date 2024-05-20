ced13dc96419658b9910efc006784b1d35e4fe2d
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-20 10:53:30 +0200

    Add files via upload
    Pusho di nuovo l'esercizio del selfwork operatore 1 js. Aspetto le altre correzioni

---
+++ main2.js
@@ -0,0 +1,13 @@
+let number1 = 101; 
+let number2 = 102; 
+let number3 = 103; 
+let number4 = 104; 
+let number5 = 105;
+
+let somma = number1 + number2 + number3 + number4 + number5;
+console.log(somma);
+
+let media = somma / number1 + number2 + number3 + number4 + number5;
+console.log(media);
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
